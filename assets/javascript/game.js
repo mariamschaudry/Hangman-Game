37d9bb97b5ce8db2c9380fe957d29f39f5f6b003
Mariam Chaudry <mariamchaudry@MacBook-Pro-2.local>
2018-03-02 18:41:50 -0500

    more updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,15 +16,15 @@ var wordArray = [
 
 // Number of wins. //
 
-var wins = [];
+var wins = 0;
 
 // Number of losses. //
 
-var losses = []; 
+var losses = 0; 
 
 // Current player guess. //
 
-var playerAnswer = [];
+var playerGuess = [];
 
 // Number of guesses left. //
 
@@ -36,21 +36,51 @@ var incorrectGuesses = [];
 
 // Where the words are displayed. //
 
-var wordDisplay = []; 
-// -------------------------------------------------------------------------------------------------------------//
+var wordDisplay = '';  
+
+
+// ------------------------------------------------------------------------------------------------------------- //
 
 // Functions //
 
-// Get a random word from the word array. //
+// Get a random word from the word array. //  
 
-var word = wordArray[Math.floor(Math.random() * wordArray.length)];
+var word = wordArray[Math.floor(Math.random() * wordArray.length)]; 
 
 console.log (word)
+
+// The selected word should be split up. //
+
+var splitWord = word.split('');
+
+console.log(splitWord)
 
 // Making the spaces for the letters to sit on. //
 
 function spaces () {
-    for (var i = 0; i < playerAnswer.length; i++) {
-        wordDisplay.push("_");
+    for (var i = 0; i < splitWord.length; i++) 
+        wordDisplay.push('_');
     }
+
+console.log(wordDisplay) 
+
+function WordDisplay () {
+    for (var m = 0; m < word.length; m++) {
+        if (word [m] === playerGuess)
+        wordDisplay[m] = word [m];
+     }
 }
+
+// --------------------------------------------------------------------------------------------------------------- //
+// The Game Play //
+
+// First the player has to choose a letter. //
+
+document.onkeyup = function (event) {
+    playerGuess = event.key; }
+
+
+// This is if the player chooses correct letters. //
+
+
+
